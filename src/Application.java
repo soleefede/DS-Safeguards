998e16ccee8cf9acb26670803782694f6fdd3b1f
emily <0918277686>
2019-08-15 00:00:44 -0700

    Last check

--- src/Application.java
+++ src/Application.java
@@ -7,10 +7,10 @@ public class Application {
 
         try {
             //Get data from input
-            FileReader fileReader = new FileReader("2.in");
+            FileReader fileReader = new FileReader("5.in");
             Scanner scanner = new Scanner(fileReader);
             int n = scanner.nextInt();
-            Node[] nodes = new Node[n*2];
+            Node[] nodes = new Node[n * 2];
             //Write entry into node in nodes array
             for (int i = 0; i < n; i++) {
                 nodes[2 * i] = new Node(scanner.nextInt(), i);
@@ -19,27 +19,41 @@ public class Application {
 
             //Sort nodes array by hour
             Arrays.sort(nodes);
+//            for (Node nn : nodes) {
+//                System.out.println("index = " + nn.index+",time = "+nn.hour);
+//            }
 
-            int[] table= new int[n];
-            for (int j=0; j<n; j++){
-                table[j]= nodes[2*j+1].hour-nodes[2*j].hour;
-            }
-            int hourCovered1 = 0;
-            for (int k=0; k<nodes.length; k++){
-                while(k<nodes.length) {
-                    hourCovered1 += nodes[k + 1].hour - nodes[k].hour;
+            int[] hourSolo = new int[n];
+            int hourCovered = 0;
+            int hourPrev = 0;
+            LinkedList<Node> indexPool = new LinkedList<>();
+
+            for (int j = 0; j < nodes.length; j++) {
+                if (indexPool.size()==1) {
+                    //System.out.println("indexPool getFirst = " + indexPool.getFirst());
+                    hourSolo[indexPool.getFirst().index] += nodes[j].hour - hourPrev;
                 }
+                if (!indexPool.isEmpty()) {
+                    hourCovered += nodes[j].hour - hourPrev;
+                }
+                if (indexPool.contains(nodes[j])){
+                    indexPool.remove(nodes[j]);
+                }else {
+                    indexPool.add(nodes[j]);
+                }
+                hourPrev = nodes[j].hour;
+                Collections.sort(indexPool);
+                //System.out.println("indexPool sort = " + indexPool);
             }
 
-            int result1 = 0;
-            System.out.println(hourCovered1);
-            for (int entry: table) {
-                result1 = Math.max(result1, hourCovered1 - entry);
+            int result = 0;
+            for (int entry : hourSolo) {
+                result = Math.max(result, hourCovered - entry);
             }
-            System.out.println(result1);
+            System.out.println(result);
             //Write answer to output
-            FileWriter fileWriter = new FileWriter("1.out");
-            fileWriter.write(String.valueOf(result1));
+            FileWriter fileWriter = new FileWriter("5.out");
+            fileWriter.write(String.valueOf(result));
             fileWriter.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -59,6 +73,11 @@ public class Application {
 
         public int compareTo(Node node) {
             return hour - node.hour;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            return this.index == ((Node)o).index;
         }
 
     }
